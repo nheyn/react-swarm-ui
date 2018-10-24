5ae892831ebb8414e75d820550c8022efeb82872
Nick Heyn <nick@heyn.io>
2018-10-24 17:42:08 -0600

    Update 'program' to hold zooids in place

--- src/index.js
+++ src/index.js
@@ -3,18 +3,15 @@ import ZooidManager from './ZooidManager';
 
 const manager = new ZooidManager('http://localhost:9092');
 
-const unsub = manager.subscribe(async () => {
-    unsub();
-
+manager.subscribe(async () => {
     try {
       await manager.setState((state) => ({
         ...state,
-        zoo: state.zoo.map((zooid) => zooid.id === 0? ({
+        zoo: state.zoo.map((zooid) => ({
           ...zooid,
-          des: [ state.dim[0] / 2, state.dim[1] / 2 ],
-        }): zooid),
+          des: zooid.pos,
+        })),
       }));
-      console.log('sent');
     } catch(err) {
       console.error(err);
     }
